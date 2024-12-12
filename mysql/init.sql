76f59224588df0b979aee0b2c208e56eba32af65
Sumit2606 <119752421+Sumit2606@users.noreply.github.com>
2024-12-12 11:08:41 +0530

    Update init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,8 +1,15 @@
-CREATE DATABASE registration;
-USE registration;
+CREATE DATABASE SurveyDB;
 
-CREATE TABLE users (
+USE SurveyDB;
+
+CREATE TABLE SurveyResponses (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
+    email VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    recommend VARCHAR(50) NOT NULL,
+    languages TEXT,
+    comments TEXT,
+    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
